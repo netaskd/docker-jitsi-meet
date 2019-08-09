9dcdbc0ef99e469d3c346c6614ae5b2778541c27
netaskd <netaskd@gmail.com>
2019-08-09 18:15:23 +0300

    prosody: fix from PR #133

--- prosody/rootfs/defaults/conf.d/jitsi-meet.cfg.lua
+++ prosody/rootfs/defaults/conf.d/jitsi-meet.cfg.lua
@@ -1,5 +1,5 @@
 admins = { "{{ .Env.JICOFO_AUTH_USER }}@{{ .Env.XMPP_AUTH_DOMAIN }}" }
-plugin_paths = { "/prosody-plugins-custom" }
+plugin_paths = { "/prosody-plugins/", "/prosody-plugins-custom" }
 http_default_host = "{{ .Env.XMPP_DOMAIN }}"
 
 VirtualHost "{{ .Env.XMPP_DOMAIN }}"
